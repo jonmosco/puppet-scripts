3143fa2b98dbe6bfc5b9454dc1eee92ee9bd2b7f
Jon Mosco <jonny.mosco@gmail.com>
2015-05-23 21:36:49 -0400

    start puppet not puppet-agent

--- install_puppet_4_deb.sh
+++ install_puppet_4_deb.sh
@@ -11,13 +11,13 @@ if `lsb_release -a` | grep -e "14" &> /dev/null; then
   dpkg -i  $PUPPET_REPO_14
   apt-get update
   apt-get install -y puppet-agent
-  update-rc.d puppet-agent defaults
+  update-rc.d puppet defaults
 elif `lsb_release -a` | grep -e "12" &> /dev/null; then
   /usr/bin/wget http://apt.puppetlabs.com/${PUPPET_REPO_12}
   dpkg -i $PUPPET_REPO_12
   apt-get update
   apt-get install -y puppet-agent
-  update-rc.d puppet-agent defaults
+  update-rc.d puppet defaults
 else
   echo "Unsupported Version of Ubuntu Linux"
 fi
