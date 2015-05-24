1e5fdd21f3a098ceb498953ce6d48fa8a0eb9ff4
Jon Mosco <jonny.mosco@gmail.com>
2015-05-23 21:34:16 -0400

    added apt-get update for package refresh

--- install_puppet_4_deb.sh
+++ install_puppet_4_deb.sh
@@ -9,11 +9,13 @@ PUPPET_REPO_12=puppetlabs-release-pc1-precise.deb
 if `lsb_release -a` | grep -e "14" &> /dev/null; then
   /usr/bin/wget http://apt.puppetlabs.com/${PUPPET_REPO_14}
   dpkg -i  $PUPPET_REPO_14
+  apt-get update
   apt-get install -y puppet-agent
   update-rd.d puppet-agent defaults
 elif `lsb_release -a` | grep -e "12" &> /dev/null; then
   /usr/bin/wget http://apt.puppetlabs.com/${PUPPET_REPO_12}
   dpkg -i $PUPPET_REPO_12
+  apt-get update
   apt-get install -y puppet-agent
   update-rd.d puppet-agent defaults
 else
